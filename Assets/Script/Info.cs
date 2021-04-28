a5cef5b3449e5a78de6a6cf14ba0fb892f98c1d6
sj-jason-liu <sj.jason.liu@gmail.com>
2021-04-28 23:28:20 +0800

    Add script folder.

---
+++ Assets/Script/Info.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    public string myName = "Jason";
+    public int myAge = 31;
+    bool isAwake = true;
+    [Space]
+    private float myWeight = 75.6f;
+    public int foodWeight = 3;
+    [Space]
+    public int bill = 40;
+    public float tipRate = 20f;
+    public float totalAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float tip = bill * (tipRate / 100);
+        totalAmount = bill + (bill * (tipRate / 100));
+        Debug.Log("Total amount is " + totalAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
