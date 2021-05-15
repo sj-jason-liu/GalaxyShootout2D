fcdedd63aec9c417994fe432606ae8c7d57e7e9a
sj-jason-liu <sj.jason.liu@gmail.com>
2021-05-15 23:59:56 +0800

    Add an astroid & explosion effect.(Not finish yet)

---
+++ Assets/Script/Astroid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    [SerializeField]
+    private float _rotateSpeed = 3f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if collide with laser
+        //instantiate the explosion animation at the location
+        //destroy after the animation end
+        if(other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
