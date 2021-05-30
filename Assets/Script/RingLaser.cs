49bdd72404d88fc87cfd111aadf864a42f3e116c
sj-jason-liu <sj.jason.liu@gmail.com>
2021-05-30 13:28:16 +0800

    Created ring laser sprites and script(Not finish yet).

---
+++ Assets/Script/RingLaser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingLaser : MonoBehaviour
+{
+    [SerializeField]
+    private float _scaleSpeed = 1f;
+    private Vector3 _scaleRange;
+
+    private void Start()
+    {
+        _scaleRange = new Vector3(_scaleSpeed, _scaleSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Time.deltaTime * _scaleRange;
+        if(transform.localScale.x > 4)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
